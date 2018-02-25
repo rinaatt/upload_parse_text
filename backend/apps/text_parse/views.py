ac9796e2ab864b39717676844d2c43b04639ff53
Rinat R. Akhtamov <rinaatt@gmail.com>
2018-02-26 02:19:30 +0500

    Changed uploading file

--- backend/apps/text_parse/views.py
+++ backend/apps/text_parse/views.py
@@ -2,6 +2,7 @@ import logging
 from django.views.generic import TemplateView, View
 from django.http.response import JsonResponse
 
+
 log = logging.getLogger('app.text_parse')
 
 
@@ -12,7 +13,6 @@ class IndexView(TemplateView):
 class UploadView(View):
 
     def post(self, request, *args, **kwargs):
-        file_data = request.POST['data']
-        file_name = request.POST['name']
-        log.debug('Get file with length: %s', len(file_data))
-        return JsonResponse({'result': 'OK'})
+        file = request.FILES['file']
+        log.debug('Got file: %r with size: %d', file, file.size)
+        return JsonResponse({'result': 'OK', 'fileSize': file.size})
